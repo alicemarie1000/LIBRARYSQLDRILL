dbf557d9b258ba039a7a4fe529225141294b9e14
alicemarie1000 <alicemarie1000@users.noreply.github.com>
2016-12-09 13:14:31 -0500

    Add files via upload

--- SQLQuery7.sql
+++ SQLQuery7.sql
@@ -1,21 +1,19 @@
 USE LBRYDB
 
-Select LB.BranchName FROM LIBRARY_BRANCH AS LB
-INNER Join Book_COPIES AS BC
-On LB.BranchId = BC.BranchId
-Full OUTER Join BOOK_AUTHORS AS BA
-ON BC.BookId = BA.BookID
-WHERE BA.AuthorName = 'Stephen King'
+Select BK.Title FROM Book AS BK
+Inner Join Book_Authors AS BA
+ON BK.BookID = BA.BookId
+Inner Join Book_Copies AS BC
+On BK.BookId = BC.BookId
+Where BA.AuthorName = 'Stephen King'
+AND BC.BranchId = '2'
 
+/* Update the following for the query to work
+Update BOOK_Copies
+Set BranchId = 2
+Where BookID = '8';
 
+Update Book_Copies
+Set BookId = '8'
+Where BranchId = '2';*/
 
-SELECT * FROM Library_Branch
-Select * FROM Book_Copies
-
-S'
-
-
-
-Update Book_Loans
-Set DueDate = 12072016
-WHERE BranchID = 1
