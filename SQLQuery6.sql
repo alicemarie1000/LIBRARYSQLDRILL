2fd87208da0a08aeafa6f8f5143cb636ce488bb0
alicemarie1000 <alicemarie1000@users.noreply.github.com>
2016-12-09 20:23:36 -0500

    Add files via upload
    Added count query

--- SQLQuery6.sql
+++ SQLQuery6.sql
@@ -1,10 +1,12 @@
 USE LBRYDB
 
-SELECT BORROWER.Name, Count(Borrower.Name)FROM
+SELECT BORROWER.Name, Count(Borrower.Name)TotalCheckedOut FROM
 Borrower
 INNER JOIN BOOK_LOANS
 ON Borrower.CardNo = BOOK_LOANS.CardNo
 Group By Borrower.Name
+HAVING Count(*) > 4
 
-
+Select * FROM Borrower
+Select * FROM Book_Loans
 
